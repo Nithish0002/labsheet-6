0729ab5f33a7fd9021875248b65734ead2693442
Nithish0002 <pnithish958@gmail.com>
2026-04-01 11:58:10 +0530

    Add AddNumbers class to calculate sum of two integers

---
+++ AddNumbers.java
@@ -0,0 +1,9 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 25;
+
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
